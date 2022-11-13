359d269f738ac38f19971fd932eecc66e2454c8c
NIGMA MEN <107373065+AdrianVeas@users.noreply.github.com>
2022-11-13 11:13:40 -0500

    recetas

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -8,7 +8,8 @@
                     Departamento Médico, UNIVERSIDAD TÉCNICA ESTATAL DE QUEVEDO
                 </div>
                 <div>
-                    Desarrollado por Geophantom: <span data-toggle="tooltip" data-placement="top" title="bryand.mendoza@uteq.edu.ec">Bryan Mendoza</span> - <span data-toggle="tooltip" data-placement="top" title="rafael.guerrero2016@uteq.edu.ec">Rafael Guerrero</span> - <span data-toggle="tooltip" data-placement="top" title="miguel.suarez2016@uteq.edu.ec">Miguel Suárez</span>
+                    Desarrollado por: <span data-toggle="tooltip" data-placement="top" title="bryand.mendoza@uteq.edu.ec">Bryan Mendoza</span> - <span data-toggle="tooltip" data-placement="top" title="rafael.guerrero2016@uteq.edu.ec">Rafael Guerrero</span> - <span data-toggle="tooltip" data-placement="top" title="miguel.suarez2016@uteq.edu.ec">Miguel Suárez</span>
+                    - <span data-toggle="tooltip" data-placement="top" title="adrian.veas2016@uteq.edu.ec">Adrián Veas</span> - <span data-toggle="tooltip" data-placement="top" title="eduardo.galvez@uteq.edu.ec">Eduardo Galvez</span> - <span data-toggle="tooltip" data-placement="top" title="diego.capurro2016@uteq.edu.ec">Diego Capurro</span>
                 </div>
             </div>
         </div>
